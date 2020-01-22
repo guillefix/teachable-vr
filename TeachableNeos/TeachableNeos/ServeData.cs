a60f91e723c737b4cda1eff463d07b331588b480
Guillermo Valle <guillefix@gmail.com>
2020-01-22 13:03:19 +0100

    neos plugin boilerplate

---
+++ TeachableNeos/TeachableNeos/ServeData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseX;
+using FrooxEngine;
+//using FrooxEngine.LogiX;
+
+using Grpc.Core;
+using Grpc.Core.Utils;
+//using LogiX;
+using FrooxEngine.LogiX;
+
+namespace ServeData
+{
+    [Category("LogiX/Network")]
+    [NodeName("Serve data")]
+    public class ServeData : LogixNode
+    {
+        public readonly Input<float> TestNumber;
+        public readonly Input<float> TestNumber2;
+
+        public override void RunStartup()
+        {
+
+        }
+
+        protected override void OnChanges()
+        {
+
+        }
+
+        //[ImpulseTarget]
+        //public void Run()
+        //{
+
+        //}
+    }
+}
